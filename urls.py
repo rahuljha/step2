4a03d2cc077134f194258f9a7bc7e11e2cb6cfef
rahuljha <rahuljha@whoelse.xen.prgmr.com>
2010-05-29 10:24:12 +0000

    initial commit

---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+                       (r'^admin/', include(admin.site.urls)),
+    # Example:
+    # (r'^step2/', include('step2.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+)
