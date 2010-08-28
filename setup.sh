54ef433b4da20b0f8aeee6e47cf2466743a2fc03
rdsr <rdsr.me@gmail.com>
2010-08-28 18:17:37 +0530

    added missing pieces for permissions for projects to work

--- setup.sh
+++ setup.sh
@@ -6,6 +6,5 @@ sudo easy_install -U markdown
 sudo easy_install -U django-notification
 # dependencies required by django-forum (http://code.google.com/p/django-forum/)
 sudo easy_install -U django-registration
+sudo easy_install -U django-authority
 # flatpages comes with django itself I guess
-
-
